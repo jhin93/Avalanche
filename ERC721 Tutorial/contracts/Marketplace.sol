02d0ef8087d97e1c42c1b615efe32c2ff4c6dbb3
jhin93 <scene1993@gmail.com>
2022-07-13 15:48:51 +0900

    listItem function

--- ERC721 Tutorial/contracts/Marketplace.sol
+++ ERC721 Tutorial/contracts/Marketplace.sol
@@ -37,16 +37,16 @@ contract Marketplace is Collectible {
     }
 
     function listItem(uint256 tokenId, uint256 price) public onlyTokenOwner(tokenId) 
-    {
-        require(!hasBeenListed[tokenId], "The token can only be listed once");
-        _transfer(msg.sender, address(this), tokenId);
-        claimableByAccount[tokenId] = msg.sender;
+    {   // The function takes two parameters, namely the token id and the price. 
+        require(!hasBeenListed[tokenId], "The token can only be listed once"); // We begin by defining the constraints which are that only the token owner can list the NFT and that this NFT has not been listed already.
+        _transfer(msg.sender, address(this), tokenId); // Then we proceed by using the _transfer(msg.sender, address(this), tokenId) function, which is provided by the ERC721.sol contract, and we transfer the token to the Marketplace.sol contract.
+        claimableByAccount[tokenId] = msg.sender; // Then we specify the msg.sender as the address that can cancel the listing and we update the mappings accordingly by creating a new Listing
         tokenIdToListing[tokenId] = Listing(
             price,
             msg.sender
         );
-        hasBeenListed[tokenId] = true;
-        emit ItemListed(tokenId, price, msg.sender);
+        hasBeenListed[tokenId] = true; // and by specifying that the token id has been listed.
+        emit ItemListed(tokenId, price, msg.sender); // At the end, as usual, we emit an event.
     }
 
 }
